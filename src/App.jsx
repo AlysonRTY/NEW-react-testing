980c8d8064cfe8a788d1b9c0bb81bb8aeaff9f5d
AlysonRTY <variationsforpiano@gmail.com>
2025-03-10 16:00:25 +0100

    monday commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function MyButton() {
+
+
+
+
+
+
+
+
+function MyButton({ count, onClick }) {
+  // const [count, setCount] = useState(0);
+  // function handleClick() {
+  //   setCount(count + 1);
+  // }
   return (
-    <>
-   <h1>coolcool</h1>
-      <button>Im a Butto<br />now do you do?</button>
-      <br />
-      <h3>yoyoy
-      </h3>
-    </>
-    
-  );
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  )
 }
-const products1 = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
-];
-const products2 = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-const listItems1 = products1.map(product =>
-  <li key={product.id}>
-    {product.title}
-  </li>
-);
-const listItems2 = products2.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-);
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([])
 
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+  const fetchData = () => {
+    fetch("https://rickandmortyapi.com/api/character/").then(res => {
+      return res.json()
+    }).then(result => {
+      setData(result.results)
+      console.log(result.results);
+    })
+  }
+  fetchData()
+}, [])
+  
+  
+  
+
+ const charInfo = data.map(person =>
+   <li key={person.id}>
+     <p>
+        <b>{person.name}:</b>
+      </p>
+      <img
+        src={person.image}
+        alt={person.name}
+      />
+    </li>
+ );
+  
+  
+  
+  
+  
+  
+  
   
 
   return (
     <>
-      <h1>Hello, World!</h1>
+      <h1>Hello Friends!</h1>
       <p>Welcome to my very simple React app.</p>
+      <MyButton count={count} onClick={handleClick} /><br />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
       <MyButton />
-      <MyButton />
-      <div>
-        <ul>{listItems1}</ul>
-        <ul>{listItems2}</ul>
+      {/* <MyButton /><br /><MyButton /><br /><MyButton /> */}
+      {/* {data.map((element) => {
+        return (
+          <div key={element.name}>
+            <p>{element.name}</p>
+            </div>
+        )
+      })} */}
+      {/* {data.map((element) => {
+        return (
+          <li key={element.data}>
+            <p>{element.name}</p>
+              <img src={element.image}></img>   
+            </li>
+        )
+      })} */}
+      
+      <ul className="no-bullets">{charInfo}</ul>
 
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-     
+
+
     </>
   )
 }
