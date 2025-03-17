061eb2a6b88bfda1cec0faaabfea20aea96bed26
AlysonRTY <variationsforpiano@gmail.com>
2025-03-17 13:54:01 +0100

    page 2 commit

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,9 +1,41 @@
-
+import { useState, useEffect, } from 'react'
 
 
 
 
 export default function Page2() {
+    const [data, setData] = useState([])
+    const [search, setSearch] = useState(" ")
+
+
+
+    useEffect(() => {
+        const fetchData = () => {
+            fetch("https://rickandmortyapi.com/api/character/?page=2").then(res => {
+                return res.json()
+            }).then(result => {
+                setData(result.results)
+                console.log(result.results);
+            })
+        }
+        fetchData()
+    }, [])
+
+
+    const filtererCharacters = data.filter((person) => {
+        if (search === "") {
+            return true;
+        }
+        return person.name.toLowerCase().includes(search.toLowerCase());
+    })
+
+
+    console.log('filteredCharacters :>> ', filtererCharacters);
+
+
+
+
+
     return (
         <h2>hello</h2>
     )
